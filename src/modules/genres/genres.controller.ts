927ce0e2c77ea7023af0dc6eb7465b1be2e474c5
Lorena Sineiro <losingar@gmail.com>
2024-07-21 18:35:25 +0200

    first simple resource coded

--- src/modules/genres/genres.controller.ts
+++ src/modules/genres/genres.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { GenresService } from '@modules/genres/services/genres.service';
+import { Genre } from '@entities/genre.entity';
 
 @Controller('genres')
 export class GenresController {
@@ -8,5 +9,25 @@ export class GenresController {
   @Get(``)
   public getList() {
     return this._genresService.getList();
+  }
+
+  @Get(`/:id`)
+  public getById(@Param(`id`) id: string) {
+    return this._genresService.getById(id);
+  }
+
+  @Post(``)
+  public create(@Body() genre: Partial<Genre>) {
+    return this._genresService.create(genre.name);
+  }
+
+  @Put(`/:id`)
+  public update(@Param(`id`) id: string, @Body() genre: Genre) {
+    return this._genresService.update(id, genre);
+  }
+
+  @Delete(`/:id`)
+  public delete(@Param(`id`) id: string) {
+    return this._genresService.delete(id);
   }
 }
--- src/modules/genres/services/genres-query-builder/genres-query-builder.service.ts
+++ src/modules/genres/services/genres-query-builder/genres-query-builder.service.ts
@@ -1,13 +1,26 @@
 import { Genre } from '@entities/genre.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, InsertResult, Repository, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class GenresQueryBuilderService {
   constructor(@InjectRepository(Genre) private readonly _genresRepo: Repository<Genre>) {}
 
-  async getList() {
-    return await this._genresRepo.find({ order: { name: 'ASC' } });
+  get(id?: string): Promise<Genre[]> {
+    if (id) return this._genresRepo.find({ where: { id: id } });
+    return this._genresRepo.find({ order: { name: 'ASC' } });
+  }
+
+  create(id: string, name: string): Promise<InsertResult> {
+    return this._genresRepo.insert(new Genre(id, name));
+  }
+
+  update(id: string, genre: Genre): Promise<UpdateResult> {
+    return this._genresRepo.update(id, genre);
+  }
+
+  delete(id: string): Promise<DeleteResult> {
+    return this._genresRepo.delete(id);
   }
 }
