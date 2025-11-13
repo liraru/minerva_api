361fd5a6ad6c5920869d216b4cb4e85a2751fc81
Lorena Sineiro <losingar@gmail.com>
2025-11-13 09:54:56 +0100

    swagger config for genres controller

--- src/modules/genres/genres.controller.ts
+++ src/modules/genres/genres.controller.ts
@@ -3,6 +3,33 @@ import { GenresService } from '@modules/genres';
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+const API_RESPONSE = {
+  GET_LIST: {
+    status: 200,
+    description: 'Result',
+    example: [{ id: 'uuid', name: 'string' }]
+  },
+  GET_BY_ID: {
+    status: 200,
+    description: 'Result',
+    example: { id: 'uuid', name: 'string' }
+  },
+  CREATE: {
+    status: 200,
+    description: 'Result',
+    example: { id: 'uuid', name: 'string' }
+  },
+  UPDATE: {
+    status: 200,
+    description: 'Result',
+    example: { id: 'uuid', name: 'string' }
+  },
+  DELETE: {
+    status: 200,
+    description: 'Result'
+  }
+};
+
 @ApiTags('genres')
 @Controller('genres')
 export class GenresController {
@@ -10,27 +37,35 @@ export class GenresController {
 
   @Get(``)
   @ApiOperation({ summary: 'Get all genres' })
-  @ApiResponse({ status: 200, description: '', example: [{}] })
+  @ApiResponse(API_RESPONSE.GET_LIST)
   public getList() {
     return this._genresService.getList();
   }
 
   @Get(`/:id`)
+  @ApiOperation({ summary: 'Get genre by id' })
+  @ApiResponse(API_RESPONSE.GET_BY_ID)
   public getById(@Param(`id`) id: string) {
     return this._genresService.getById(id);
   }
 
   @Post(``)
+  @ApiOperation({ summary: 'Create genre' })
+  @ApiResponse(API_RESPONSE.CREATE)
   public create(@Body() genre: Partial<Genre>) {
     return this._genresService.create(genre.name);
   }
 
   @Put(`/:id`)
+  @ApiOperation({ summary: 'Update genre' })
+  @ApiResponse(API_RESPONSE.UPDATE)
   public update(@Param(`id`) id: string, @Body() genre: Genre) {
     return this._genresService.update(id, genre);
   }
 
   @Delete(`/:id`)
+  @ApiOperation({ summary: 'Delete genre' })
+  @ApiResponse(API_RESPONSE.DELETE)
   public delete(@Param(`id`) id: string) {
     return this._genresService.delete(id);
   }
